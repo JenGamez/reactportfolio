4d6a1e5455328cd8eb02eba0f7575ce59f852430
Jennifer Gamez <jenggamez@gmail.com>
2020-02-27 14:43:07 -0800

    got image loaded

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,8 @@ import React from "react";
 import Col from "../components/Col";
 import Container from "../components/Container";
 import Row from "../components/Row";
+import Img from "../img/JenGamez.jpg";
+import "../styles/Img.css";
 
 
 function About() {
@@ -11,6 +13,10 @@ function About() {
                 <Row>
                     <Col size="md-3">
 <h1>Jen Gamez</h1>
+
+<div>
+                        <img src={Img} alt="JenGamez" class="jengamez"></img>
+                    </div>
 
 
                     </Col>
